984c522e6aaf1736f3222b82c356daaaf95fed13
yao-xiyan <2450998207@qq.com>
2019-09-21 12:00:42 +0800

    打开关闭评论 加载标题

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,21 +1,36 @@
 <template>
   <el-card>
-      <!-- 面包屑组件 -->
-      <!-- slot => header 是给el-card的插槽 -->
-      <bread-crumb slot="header">
-        <!-- solt => 给面包屑的插槽 -->
-        <template slot='title'>评论列表</template>
-      </bread-crumb>
-      <el-table :data="list">
-          <el-table-column lable="标题" prop="title"></el-table-column>
-          <el-table-column :formatter="stateformatter" align="center" lable="评论状态" prop="comment_status"></el-table-column>
-          <el-table-column align="center" lable="总评论数" prop="total_comment_count"></el-table-column>
-          <el-table-column align="center" lable="粉丝评论数" prop="fans_comment_count"></el-table-column>
-          <el-table-column align="center" lable="操作">
-              <el-button size="small" type="text">修改</el-button>
-              <el-button size="small" type="text">关闭</el-button>
-          </el-table-column>
-      </el-table>
+    <!-- 面包屑组件 -->
+    <!-- slot => header 是给el-card的插槽 -->
+    <bread-crumb slot="header">
+      <!-- solt => 给面包屑的插槽 -->
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <!-- data 属性是一个数组 -->
+    <el-table :data="list">
+         <!-- el-table-column 列 属性 prop => 键名 => 字段名 label => 表头 -->
+      <el-table-column width="600px" label="标题" prop="title"></el-table-column>
+      <el-table-column
+        :formatter="stateFormatter"
+        align="center"
+        lable="评论状态"
+        prop="comment_status"
+      ></el-table-column>
+      <el-table-column align="center" label="总评论数" prop="total_comment_count"></el-table-column>
+      <el-table-column align="center" label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+      <el-table-column align="center" label="操作">
+        <!-- 作用域插槽 => 接收 el-table-column row/column/index/  -->
+        <template slot-scope="obj">
+          <!-- 自定义内容 获取其他列的内容 -->
+          <el-button size="small" type="text">修改</el-button>
+          <el-button
+            @click="btSwitch(obj.row)"
+            size="small"
+            type="text"
+          >{{ obj.row.comment_status ? '关闭评论' : '打开评论' }}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
@@ -37,16 +52,35 @@ export default {
       })
     },
     // 返回的数据给list
-    stateformatter (row, column, cellValue, index) {
+    stateFormatter (row, column, cellValue, index) {
       return cellValue ? '正常' : '关闭'
     },
-    created () {
-      this.getComment()
+    // 打开或关闭 row 代表当前行的所有数据
+    // 需要两个参数  （id，）
+    btSwitch (row) {
+      // 使用 弹窗 组件
+      let msg = row.comment_status ? '关闭评论' : '打开评论'
+      this.$confirm(`您确定要永久删除${msg}评论?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => { // 确定调用接口
+        this.$axios({
+          url: 'comments/status',
+          method: 'put',
+          params: { article_id: row.id }, // 路径参数
+          data: { allow_comment: !row.comment_status }
+        }).then(() => {
+          this.getComment() // 重新拉取数据
+        })
+      })
     }
+  },
+  created () {
+    this.getComment()
   }
 }
 </script>
 
 <style>
-
 </style>
