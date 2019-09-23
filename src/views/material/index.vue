a3d7bdc518bcb007bc1898f022c8557503469cb6
yao-xiyan <2450998207@qq.com>
2019-09-23 09:04:27 +0800

    素材列表 - 数据加载

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,15 +1,88 @@
 <template>
-  <el-card>
-
+  <el-card v-loding="loading">
+    <bread-crumb slot="header">
+      <!-- solt => 给面包屑的插槽 -->
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!-- el-tabs => el-tab-pane -->
+    <el-tabs v-model="activeName" @tab-click="getMaterial">
+      <el-tab-pane label="全部素材" name="all">
+        <!-- 全部素材 -->
+        <div class="img-list">
+          <el-card class="img-item" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <div class="operate">
+              <i :style="{color: item.is_collected ? 'red' : '#000'}" class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </div>
+          </el-card>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <div class="img-list">
+          <el-card class="img-item" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      loading: false,
+      activeName: 'all',
+      list: []
+    }
+  },
+  methods: {
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: { collect: this.activeName === 'collect' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.img-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+   .img-item {
+       width: 180px;
+       height: 180px;
+       border-radius: 6px;
+       margin:30px;
+       position: relative;
+       img {
+           width:100%;
+           height:100%;
+       }
+       .operate {
+           background-color: #f4f5f6;
+           position: absolute;
+           left:0;
+           bottom:0;
+           width: 100%;
+           height:30px;
+           display: flex;
+           justify-content: space-around;
+           align-items: center;
+           i {
+               font-size:18px;
+           }
+       }
+   }
+}
 </style>
