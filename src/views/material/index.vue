1181d66b7c312510e4f84135c380bf6c66554a5a
yao-xiyan <2450998207@qq.com>
2019-09-23 10:16:46 +0800

    素材列表 - 渲染列表未完成上传

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <el-card v-loding="loading">
+  <el-card v-loading="loading">
+    <!-- header具名是给卡片的 -->
     <bread-crumb slot="header">
-      <!-- solt => 给面包屑的插槽 -->
+      <!-- title具名 是面包屑组件的具名 -->
       <template slot="title">素材管理</template>
     </bread-crumb>
+    <!-- http-request 自定义上传 -->
+    <el-upload :show-file-list="false" :http-request="uploadImg" action="" class="upload">
+      <el-button type="primary">上传图片</el-button>
+    </el-upload>
     <!-- el-tabs => el-tab-pane -->
-    <el-tabs v-model="activeName" @tab-click="getMaterial">
+    <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
         <!-- 全部素材 -->
         <div class="img-list">
@@ -17,6 +22,16 @@
             </div>
           </el-card>
         </div>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+          ></el-pagination>
+        </el-row>
       </el-tab-pane>
       <el-tab-pane label="收藏素材" name="collect">
         <div class="img-list">
@@ -24,6 +39,16 @@
             <img :src="item.url" alt />
           </el-card>
         </div>
+        <el-row type="flex" justify="center">
+          <el-pagination
+            @current-change="changePage"
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+          ></el-pagination>
+        </el-row>
       </el-tab-pane>
     </el-tabs>
   </el-card>
@@ -35,16 +60,55 @@ export default {
     return {
       loading: false,
       activeName: 'all',
-      list: []
+      list: [], // 定义一个list接收数据
+      // 分页信息
+      page: {
+        total: 0,
+        currentPage: 1,
+        pageSize: 12
+      }
     }
   },
   methods: {
-    getMaterial () {
+    uploadImg (params) {
+      const data = new FormData() // 声明一个新的表单
+      data.append('image', params.file)
+      // 上传文件
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        method: 'post',
+        data
+      }).then(() => {
+        this.getMaterial()
+      })
+    },
+    // 当页码改变时 回传入一个参数
+    changePage (newMsg) {
+      this.page.currentPage = newMsg // 将最新页码赋值给currentPage
+      this.getMaterial() // 获取最新数据
+    },
+    // 切换页签的方法
+    changeTab () {
+      // 首先要把页码归1
+      this.page.currentPage = 1
+      this.getMaterial()
+    },
+    // 获取素材列表
+    getMaterial () {
+      this.loading = true
+      // this.activeName === 'collect' 相当于去找收藏的数据
+      // 如果不等于collect 相等于去找全部的数据
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: this.activeName === 'collect',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
+        }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count // 赋值总数 每次总条数都会重新赋值
+        this.loading = false
       })
     }
   },
@@ -55,34 +119,40 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.upload {
+  position: absolute;
+  right: 20px;
+  margin-top: -10px;
+}
 .img-list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-   .img-item {
-       width: 180px;
-       height: 180px;
-       border-radius: 6px;
-       margin:30px;
-       position: relative;
-       img {
-           width:100%;
-           height:100%;
-       }
-       .operate {
-           background-color: #f4f5f6;
-           position: absolute;
-           left:0;
-           bottom:0;
-           width: 100%;
-           height:30px;
-           display: flex;
-           justify-content: space-around;
-           align-items: center;
-           i {
-               font-size:18px;
-           }
-       }
-   }
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .img-item {
+    width: 180px;
+    height: 180px;
+    border-radius: 6px;
+    margin: 30px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      background-color: #f4f5f6;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 30px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      background-size: cover;
+      i {
+        font-size: 18px;
+      }
+    }
+  }
 }
 </style>
